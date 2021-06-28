9e9583e80ff5d9bceeaa1d56ceffdf13b5250692
counter0021 <ara123rus123ara@gmail.com>
2021-06-28 23:45:12 +0300

    Navbar refactor with hooks

--- front/src/components/navbar/navbar.js
+++ front/src/components/navbar/navbar.js
@@ -1,76 +1,69 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 
 import WithServices from "../hoc/with_services";
 import {addNavbarElement, setUserIsAuthenticated} from "../../redux/action";
-import GetTokenFromLocalStorage, {DeleteTokenFromLocalStorage} from "../../services/get_token_from_localstorage";
+import GetTokenFromLocalStorage from "../../services/get_token_from_localstorage";
 
 import "./navbar.scss";
 
 
 /* Навигация */
 
-class Navbar extends Component {
+const Navbar = ({Services, addNavbarElement, navbarLinks, setUserIsAuthenticated, userIsAuthenticated, active}) => {
 
-    componentDidMount() {
-        const {Services, addNavbarElement, navbarLinks, setUserIsAuthenticated} = this.props;
+    useEffect(() => {
         Services.userIsAuthenticated(GetTokenFromLocalStorage())
             .then(res => {
-                if (!res.is_authenticated) {
-                    if (!navbarLinks.find(item => item.path === '/login')) {
-                        addNavbarElement({path: '/login', body: 'Вход / Регистрация', id: 'login'});
-                    }
-                } else if (res.is_authenticated && !navbarLinks.find(item => item.path === '/logout')) {
-                    addNavbarElement({path: '/logout', body: 'Выход', id: 'logout'});
-                }
                 setUserIsAuthenticated(res.is_authenticated);
             })
             .catch(error => {
                 console.log(error);
-                DeleteTokenFromLocalStorage();  // Фикс с изменением токена в localstorage
             });
-    }
+        if (!userIsAuthenticated) {
+            if (!navbarLinks.find(item => item.path === '/login') && !GetTokenFromLocalStorage()) {
+                addNavbarElement({path: '/login', body: 'Вход / Регистрация', id: 'login'});
+            }
+        } else if (!navbarLinks.find(item => item.path === '/logout')) {
+            addNavbarElement({path: '/logout', body: 'Выход', id: 'logout'});
+        }
+    });
 
-    render() {
+    return (
+        <nav className="navbar">
+            <div className="nav">
+                {
+                    navbarLinks.map(({path, body, id}, index) => (
+                        <div key={index} className="nav__item">
+                            <Link to={path} className={"nav__link" + (active === id ? ' active' : '')}>
+                                {body}
+                            </Link>
+                        </div>
+                    ))
+                }
+            </div>
 
-        const {active, navbarLinks} = this.props;
+            <div className="nav__phone">
+                {
+                    navbarLinks.map(({path, body, id}, index) => (
+                        <div key={index} className="nav__item">
+                            <Link to={path} className={"nav__link" + (active === id ? ' active' : '')}>
+                                {body}
+                            </Link>
+                        </div>
+                    ))
+                }
+            </div>
+        </nav>
+    );
 
-        return (
-            <nav className="navbar">
-                <div className="nav">
-                    {
-                        navbarLinks.map(({path, body, id}, index) => (
-                            <div key={index} className="nav__item">
-                                <Link to={path} className={"nav__link" + (active === id ? ' active' : '')}>
-                                    {body}
-                                </Link>
-                            </div>
-                        ))
-                    }
-                </div>
-
-                <div className="nav__phone">
-                    {
-                        navbarLinks.map(({path, body, id}, index) => (
-                            <div key={index} className="nav__item">
-                                <Link to={path} className={"nav__link" + (active === id ? ' active' : '')}>
-                                    {body}
-                                </Link>
-                            </div>
-                        ))
-                    }
-                </div>
-            </nav>
-        );
-
-    }
-
-}
+};
 
 const mapStateToProps = (state) => {
     return {
+        userIsAuthenticated: state.userIsAuthenticated,
         navbarLinks: state.navbarLinks,
     };
 };
