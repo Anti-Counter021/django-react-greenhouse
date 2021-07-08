402566154c029975eaf81ec3a29d6f15f6f2618c
counter0021 <ara123rus123ara@gmail.com>
2021-07-08 18:10:04 +0300

    User admin

--- back/accounts/admin.py
+++ back/accounts/admin.py
@@ -3,4 +3,26 @@ from django.contrib import admin
 from .models import User
 
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """ Пользователи """
+
+    list_display = ('username', 'phone', 'email', 'address')
+    search_fields = ('username', 'address', 'phone', 'email')
+    fields = (
+        'username',
+        ('first_name', 'last_name'),
+        'email', 'phone', 'address',
+        'password',
+        'groups', 'user_permissions',
+        ('is_superuser', 'is_staff', 'is_active'),
+        ('date_joined', 'last_login'),
+        'orders',
+    )
+    readonly_fields = ('date_joined', 'last_login')
+    actions = None
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(User, UserAdmin)
