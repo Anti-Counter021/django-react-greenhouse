9d7337b8c512d29f9fa56d51c2d2aa508ae11303
counter0021 <ara123rus123ara@gmail.com>
2021-07-09 13:07:04 +0300

    Cart admin and feedback admin

--- back/shop/admin.py
+++ back/shop/admin.py
@@ -36,6 +36,46 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+class CartAdmin(admin.ModelAdmin):
+    """ Корзина """
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    list_display = ('id', 'owner', 'total_products', 'final_price', 'in_order', 'for_anonymous_user')
+    list_display_links = ('owner',)
+    list_filter = ('owner',)
+    empty_value_display = 'unknown'
+    search_fields = ('owner__username',)
+    fields = ('owner', 'products', ('total_products', 'final_price'), ('in_order', 'for_anonymous_user'))
+
+
+class CartProductAdmin(admin.ModelAdmin):
+    """ Товары в корзине """
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    list_display = ('cart', 'user', 'product', 'qty', 'price', 'final_price')
+    list_display_links = ('cart', 'user', 'product')
+    search_fields = ('cart__id', 'user__username', 'product__title')
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    """ Обратная связь (ошибки) """
+
+    @admin.action(description='Статус выбранных записей в "Исправлено"')
+    def fix_action(self, request, queryset):
+        queryset.update(status='fix')
+
+    list_display = ('text', 'status', 'created_at')
+    list_display_links = ('text',)
+    list_filter = ('status',)
+    search_fields = ('text',)
+    list_editable = ('status',)
+    actions = (fix_action,)
+
+
 class ProductAdmin(admin.ModelAdmin):
     """ Товары """
 
@@ -54,7 +94,7 @@ class ProductAdmin(admin.ModelAdmin):
         queryset.update(delivery_terminated=False)
 
     @admin.action(description='Скидки закончились для выбранных товаров')
-    def stop_discount(self, request, queryset):
+    def stop_discount_action(self, request, queryset):
         queryset.update(discount=0)
 
     price_with_discount.short_description = 'Цена со скидкой'
@@ -69,7 +109,7 @@ class ProductAdmin(admin.ModelAdmin):
     )
     prepopulated_fields = {'slug': ('title',)}
     inlines = (AdditionalImageProductInline, ProductFeatureInline)
-    actions = (delivery_terminated_action_true, delivery_terminated_action_false, stop_discount)
+    actions = (delivery_terminated_action_true, delivery_terminated_action_false, stop_discount_action)
 
 
 class ProductFeatureAdmin(admin.ModelAdmin):
@@ -83,22 +123,22 @@ class ReviewAdmin(admin.ModelAdmin):
     """ Отзывы """
     
     @admin.action(description='Выбранные записи изменят свои оценки на максимальные (нежелательно)')
-    def max_appraisal(self, request, queryset):
+    def max_appraisal_action(self, request, queryset):
         queryset.update(appraisal=5)
 
     list_display = ('user', 'appraisal', 'created_at')
     list_editable = ('appraisal',)
     list_filter = ('appraisal',)
     search_fields = ('user__username',)
-    actions = (max_appraisal,)
+    actions = (max_appraisal_action,)
 
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartProduct, CartProductAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductFeature, ProductFeatureAdmin)
 admin.site.register(Review, ReviewAdmin)
 
-admin.site.register(Cart)
-admin.site.register(CartProduct)
 admin.site.register(Order)
-admin.site.register(Feedback)
